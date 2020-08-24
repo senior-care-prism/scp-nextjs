fd61ae315d6020a5962d7c55fd399b1d4f9dc277
Adrian Diaz <web.art@adriandiaz.ca>
2020-08-24 15:35:18 -0400

    basic layout

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,52 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import cn from 'classnames'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Senior Care Prism</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className={cn(styles.banner, styles.columns)}>
+          <div className={cn(styles.column, styles.left)}>
+            <div className={styles.logo}>
+              <img src="images/logo-transparent.png" alt="Senior Care Prism logo" />
+            </div>
+            <div className={styles.tagline}>
+              <h1>
+              Amplifying the Voice of the Ageing Population in Minority Groups.
+              </h1>
+              <p>
+                We bridge the gap between the needs of the elderly in minority
+                groups and senior care policy. No organization is too small for
+                inclusivity, we work with small, medium and large businesses and
+                organizations in both the private and public sectors.
+              </p>
+              <button className={cn(styles.button, styles.primary)}>About Us</button>
+            </div>
+          </div>
+          <div className={styles.column}>
+            <div className={styles.navbar}>
+              <nav role="navigation">
+                <ul className={styles.columns}>
+                  <li>About Us</li>
+                  <li>Services</li>
+                  <li>Blog</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            </div>
+            <div className={styles.hero} />
+          </div>
+        </section>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
