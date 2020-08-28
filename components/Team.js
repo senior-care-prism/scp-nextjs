f9682fdfbe0f82d2db5c073f87187a33f98febfc
Adrian Diaz <web.art@adriandiaz.ca>
2020-08-27 22:31:02 -0400

    Makes bios collapsible

--- components/Team.js
+++ components/Team.js
@@ -1,7 +1,48 @@
+import { useState } from "react"
 import styles from "../styles/Team.module.scss";
 import cn from "classnames";
 
+const Collapsible = (props) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleBio = (e) => {
+    e.preventDefault();
+    console.log()
+    setExpanded(!expanded)
+  }
+  console.log("key", props.num)
+
+  const bioId = `bio-${props.num + 1}`
+  const btnId = `btn-${props.num + 1}`
+  return (
+    <>
+      <button 
+        id={btnId}
+        onClick={toggleBio}
+        type="button"
+        aria-expanded={expanded ? "true" : "false"}
+        aria-controls={bioId}
+      >
+        <span className={styles.text}>read bio </span>
+        <span className={styles.icon}>
+          <i className="ri-add-line" />
+        </span>
+      </button>
+      <p 
+        id={bioId}
+        role="region" 
+        aria-labeledby={btnId}
+        aria-label={`${props.name}'s bio`}
+        className={cn({ [styles.expanded]: expanded }, styles['team-bio'])}
+      >
+        {props.bio}
+      </p>
+    </>
+  );
+}
+
 export default function Team(props) {
+
   return (
     <section className={styles.team}>
       <div className={styles.content}>
@@ -9,8 +50,8 @@ export default function Team(props) {
           <h2>{props.heading}</h2>
         </div>
         <div className={styles.cards}>
-          {props.members.map((member) => (
-            <div className={cn(styles.member, styles.card)}>
+          {props.members.map((member, i) => (
+            <div className={cn(styles.member, styles.card)} key={member.id}>
                 <img src={`images/${member.avatar}`} alt={member.position} />
               <div className={cn(styles.container, styles.text)}>
                 <h4>
@@ -19,14 +60,8 @@ export default function Team(props) {
                 </h4>
                 <div className={styles.position}>{member.position}</div>
               </div>
-
-              <button>
-                <span>read bio </span>
-                <span>
-                  <i className="ri-add-line" />
-                </span>
-              </button>
-              <p className="team-bio">{member.bio}</p>
+              <Collapsible bio={member.bio} num={i} name={member.name}/>
+              
             </div>
           ))}
         </div>
