5d4b172481f4042801d91817f8d7efa781bc77aa
Adrian Diaz <web.art@adriandiaz.ca>
2020-09-03 16:35:46 -0400

    enable matomo

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import ThreeColumn from "../components/ThreeColumn";
 import PitchDeck from "../components/PitchDeck";
 import Team from "../components/Team";
 import Quotes from "../components/Quotes";
+import { useMatomo } from '@datapunt/matomo-tracker-react'
+import { useEffect } from 'react'
 
 
 const columnContent = [
@@ -76,21 +78,24 @@ const quotes = [
 ]
 
 export default function Home() {
+  const { trackPageView } = useMatomo()
+ 
+  React.useEffect(() => {
+    trackPageView()
+  }, [])
+
   return (
     <Layout home>
       <Head>
         <title>Senior Care Prism</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-        
-        <ThreeColumn columns={columnContent} />
-        <PitchDeck 
-          heading="We transform aging communities into inclusive communities of care."
-        />
-        <Team heading="Our Team" members={teamMembers}/>
-        <Quotes quotes={quotes} />
-      
+      <ThreeColumn columns={columnContent} />
+      <PitchDeck 
+        heading="We transform aging communities into inclusive communities of care."
+      />
+      <Team heading="Our Team" members={teamMembers}/>
+      <Quotes quotes={quotes} />
     </Layout>
   );
 }
