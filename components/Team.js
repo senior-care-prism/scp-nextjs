ea667b65041d513f46c1627f3b1ee9e2101326a9
Adrian Diaz <web.art@adriandiaz.ca>
2020-09-15 13:39:41 -0400

    remove logo band

--- components/Team.js
+++ components/Team.js
@@ -78,7 +78,7 @@ export default function Team(props) {
           ))}
         </div>
       </div>
-      <div className={styles.motif}/>
+      {/* <div className={styles.motif}/> TODO: remove CSS style*/}
     </section>
   );
 }
