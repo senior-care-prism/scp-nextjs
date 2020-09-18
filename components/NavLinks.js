a2baf88691e628a8346e6b70bfe3aaf04f03fda8
Adrian Diaz <web.art@adriandiaz.ca>
2020-09-17 23:28:22 -0400

    Nav sizing for mobile

--- components/NavLinks.js
+++ components/NavLinks.js
@@ -4,7 +4,7 @@ export default function  NavLinks(props)  {
   const navLinks = [
     {"id": 1, "to": "about-us", "scroll": true},
     {"id": 2, "to": "our-team", "scroll": true}, 
-    {"id": 3, "to": "resources", "scroll": false }, 
+    {"id": 3, "to": "pitch-deck", "scroll": true }, 
     {"id": 4, "to": "contact", "scroll": true}
   ]
   return (
@@ -12,14 +12,15 @@ export default function  NavLinks(props)  {
       <nav role="navigation">
         <ul>
           {navLinks.map((link) => (
-            <Link 
-            activeClass="active"
-            spy={true} smooth={true} offset={-100} duration={500}
-            to={link.to}
-            >
-            <li key={link.id}>
-              {link.to.replace('-',' ')}
-            </li></Link>
+            <li key={link.id} className={props.styles.navlink}>
+              <Link 
+                activeClass="active"
+                spy={true} smooth={true} offset={-100} duration={500}
+                to={link.to}
+              >
+                {link.to.replace('-',"\u00a0")}
+              </Link>
+            </li>
           ))}
         </ul>
       </nav>
