4e2ad22009560c1268a5edad78b1a50ad99a6865
Adrian Diaz <adrian.d@myplanet.com>
2021-04-10 12:19:57 -0400

    configure google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,32 @@
-/* eslint-disable react/jsx-props-no-spreading */
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import * as gtag from '../lib/gtag';
+
 import '../styles/globals.css';
 import 'remixicon/fonts/remixicon.css';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-  );
+function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  /* eslint-disable react/jsx-props-no-spreading */
+  return <Component {...pageProps} />;
 }
 
-export default MyApp;
+/* eslint-disable react/forbid-prop-types */
+App.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default App;
