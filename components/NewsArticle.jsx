05b5bf9fe5dfc7b247d352c3fbc20a0fed026ec7
Adrian Diaz <adrian.d@myplanet.com>
2021-04-12 13:23:01 -0400

    SCPS-4 disable slug page for preview

--- components/NewsArticle.jsx
+++ components/NewsArticle.jsx
@@ -2,14 +2,16 @@ import Markdown from 'markdown-to-jsx';
 import PropTypes from 'prop-types';
 
 function NewsArticle({ article }) {
+  console.log('NewsArticle', article);
+
   return (
     <section id="news-article" className={'styles.team'}>
       <div className={'styles.content'}>
         <div className={'styles.heading'}>
-          <h2>{article.headline}</h2>
+          {/* <h2>{article?.headline}</h2> */}
         </div>
         <Markdown>
-          {article.content}
+          {/* {article?.content} */}
         </Markdown>
       </div>
     </section>
--- pages/news/[slug].jsx
+++ pages/news/[slug].jsx
@@ -5,31 +5,31 @@ import NewsArticle from '../../components/NewsArticle';
 import { getNewsEntryBySlug, getAllNewsEntriesWithSlug } from '../../lib/api';
 
 export default function News({ article }) {
+  // console.log('slug.js', article);
   return (
     <Layout>
       <Head>
         <title>Senior Care Prism - News</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NewsArticle article={article} />
+      {/* <NewsArticle article={article} /> */}
     </Layout>
   );
 }
 
-export async function getStaticProps({ params }) {
-  const article = await getNewsEntryBySlug(params.slug);
-  return {
-    props: {
-      article,
-    },
-  };
-}
+// export async function getStaticProps({ params }) {
+//   const article = await getNewsEntryBySlug(params.slug);
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const articles = await getAllNewsEntriesWithSlug();
-  console.log(articles);
-  return {
-    paths: articles?.map(({ slug }) => `/news/${slug}`) ?? [],
-    fallback: true // or false // See the "fallback" section below
-  };
-}
+// export async function getStaticPaths() {
+//   const articles = await getAllNewsEntriesWithSlug();
+//   return {
+//     paths: [], //articles?.map(({ slug }) => `/news/${slug}`) ?? [],
+//     fallback: true // or false // See the "fallback" section below
+//   };
+// }
