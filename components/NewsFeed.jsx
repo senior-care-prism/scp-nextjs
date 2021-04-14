3c0b5d9460973e3341e375cdac1bc1929dbf22c5
Adrian Diaz <adrian.d@myplanet.com>
2021-04-13 22:37:33 -0400

    SCPS-4 add paginator

--- components/NewsFeed.jsx
+++ components/NewsFeed.jsx
@@ -1,29 +1,98 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-// import { useState } from 'react';
-// import cn from 'classnames';
-
+import { useState, useEffect } from 'react';
+import cn from 'classnames';
+import useWindowSize from '../hooks/useWindowSize';
 import styles from '../styles/NewsFeed.module.scss';
 
-const Card = ({ entry }) => {
+function getColumnCount(viewportWidth) {
+  if (viewportWidth < 654) {
+    return 1;
+  }
+
+  if (viewportWidth < 975) {
+    return 2;
+  }
+
+  if (viewportWidth < 1311) {
+    return 3;
+  }
+
+  return 4;
+}
+
+// function isLeftEdgeNoMargin(nodeCount, columnCount) {
+//   if (columnCount === 1) {
+//     return true;
+//   }
+//   return nodeCount % columnCount === 1;
+// }
+
+// function isRightEdgeNoMargin(nodeCount, columnCount) {
+//   return nodeCount % columnCount === 0;
+// }
+
+const Paginator = ({ pageNum, maxPage }) => {
+  const isFirstPage = pageNum === 1;
+  const isLastPage = pageNum === maxPage;
+  const previousHref = pageNum === 2
+    ? '/news'
+    : `news?p=${encodeURIComponent(pageNum - 1)}`;
+  return (
+    <nav>
+      <ul className={styles.pagination}>
+        <li className={cn(styles['page-item'], { [styles.disabled]: isFirstPage })}>
+          { isFirstPage
+            ? 'Previous'
+            : <Link href={previousHref} scroll={false}>Previous</Link>}
+        </li>
+        {Array(maxPage).fill().map((_, idx) => (
+          <Link href={idx === 0 ? '/news' : `news?p=${idx + 1}`} scroll={false} passHref>
+            <a className={cn(styles['page-item'], { [styles.active]: idx + 1 === pageNum })} href="#page">{`${idx + 1}`}</a>
+          </Link>
+        ))}
+        <li className={cn(styles['page-item'], { [styles.disabled]: isLastPage })}>
+          { isLastPage
+            ? 'Next'
+            : <Link href={`news?p=${encodeURIComponent(pageNum + 1)}`} scroll={false}>Next</Link>}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const Card = ({ entry, idx }) => {
+  const { width } = useWindowSize();
+  const columnCount = getColumnCount(width);
+  const nodeCount = idx + 1;
+
+  console.log('looo', width, columnCount);
   const articleRef = {
     pathname: '/news/[slug]',
     query: { slug: entry.slug },
   };
   return (
-    <div className={styles['news-card']}>
-      <div className={styles.content}>
+    <div
+      className={styles['news-card']}
+    >
+      <div className={styles['card-title']}>
         <h3>
           <Link href={articleRef}>
             {entry.headline}
           </Link>
         </h3>
-        <p>{entry.excerpt}</p>
-        <Link href={articleRef} className={styles.btn}>
-          Read more...
+      </div>
+      <div className={styles.content}>
+        <p className={styles.excerpt}>{entry.excerpt}</p>
+      </div>
+      <div className={styles['card-end']}>
+        <Link href={articleRef}>
+          Read&nbsp;more
         </Link>
-        <div>{moment(entry.publishedDate).format('LL')}</div>
+        <div className={styles['published-date']}>
+          {moment(entry.publishedDate).format('LL')}
+        </div>
       </div>
     </div>
   );
@@ -40,18 +109,19 @@ Card.propTypes = {
   }).isRequired,
 };
 
-function NewsFeed({ entries }) {
+function NewsFeed({ entries, pageNum, maxPage }) {
   return (
     <section id="news-feed" className={styles['news-feed']}>
       <div className={styles.content}>
-        <div className={styles.heading}>
+        <div className={styles['section-heading']}>
           <h2>News Feed</h2>
         </div>
         <div className={styles['card-container']}>
-          {entries.map((entry) => (
-            <Card entry={entry} key={entry.id} />
+          {entries.map((entry, idx) => (
+            <Card entry={entry} key={entry.id} idx={idx} />
           ))}
         </div>
+        <Paginator pageNum={pageNum} maxPage={maxPage} />
       </div>
     </section>
   );
--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,26 +1,29 @@
 import Head from 'next/head';
-
 import Layout from '../../components/Layout';
 import NewsFeed from '../../components/NewsFeed';
-import { getNewsEntries } from '../../lib/api';
+import { getNewsEntries, getNewsMaxPage } from '../../lib/api';
 
-export default function News({ newsEntries }) {
+export default function News({ newsEntries, pageNum, maxPage }) {
   return (
     <Layout>
       <Head>
         <title>Senior Care Prism - News</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NewsFeed entries={newsEntries} />
+      <NewsFeed entries={newsEntries} pageNum={pageNum} maxPage={maxPage} />
     </Layout>
   );
 }
 
-export async function getStaticProps({ params }) {
-  const newsEntries = await getNewsEntries();
+export async function getServerSideProps({ query }) {
+  const pageNum = query.p !== undefined ? parseInt(query.p, 10) : 1;
+  const newsEntries = await getNewsEntries({ pageNum });
+  const maxPage = await getNewsMaxPage({ limitPerPage: 8 });
   return {
     props: {
       newsEntries,
+      pageNum,
+      maxPage,
     },
   };
 }
