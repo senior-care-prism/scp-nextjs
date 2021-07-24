d120991329b2bec0fba938a278a9acb0a8ef27f0
Adrian Diaz <adrian.d@myplanet.com>
2021-07-24 13:46:56 -0400

    SCPS-4 Update navlinks

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 function NavLinks({ styles }) {
   const navLinks = [
     { id: 1, to: 'about-us', scroll: true },
-    { id: 2, to: 'our-team', scroll: true },
-    { id: 3, to: 'contact', scroll: true },
+    { id: 2, to: 'news', scroll: true },
+    { id: 3, to: 'our-team', scroll: true },
+    { id: 4, to: 'contact', scroll: true },
   ];
   return (
     <div className={styles.navbar}>
