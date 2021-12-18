c180804b722164e32b9a207dd1691329bde95f24
Oussama El Zein <oussama.elzein@gmail.com>
2021-12-18 04:22:20 -0800

    Update _app.jsx fixing Layout import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,30 +2,35 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import * as gtag from '../lib/gtag';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 import '../styles/globals.css';
 import 'remixicon/fonts/remixicon.css';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function App({ Component, pageProps }) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+	const router = useRouter();
+	useEffect(
+		() => {
+			const handleRouteChange = (url) => {
+				gtag.pageview(url);
+			};
+			router.events.on('routeChangeComplete', handleRouteChange);
+			return () => {
+				router.events.off('routeChangeComplete', handleRouteChange);
+			};
+		},
+		[
+			router.events
+		]
+	);
 
-  /* eslint-disable react/jsx-props-no-spreading */
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+	/* eslint-disable react/jsx-props-no-spreading */
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }
 
 /* eslint-disable react/forbid-prop-types */
