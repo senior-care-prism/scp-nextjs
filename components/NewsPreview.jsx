cb7097219f9c41dc7c5fa30a24fa24f264a0dee1
Oussama El Zein <oussama.elzein@gmail.com>
2022-01-03 20:53:34 -0800

    News preview on mobile renders as slider

--- components/NewsPreview.jsx
+++ components/NewsPreview.jsx
@@ -2,10 +2,20 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import ArticleCard from './ArticleCard';
 import styles from '../styles/NewsPreview.module.scss';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import { faCaretSquareLeft, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 function NewsPreview({ newsEntries }) {
   const renderedEntries = (newsEntries || []).map((newsEntry) => (
     <ArticleCard key={newsEntry.id} entry={newsEntry} />
+  ));
+
+  const renderedSlides = (newsEntries || []).map((newsEntry, idx) => (
+    <Slide index={idx}>
+      <ArticleCard key={newsEntry.id} entry={newsEntry} />
+    </Slide>
   ));
 
   return (
@@ -13,6 +23,26 @@ function NewsPreview({ newsEntries }) {
       <h2>Latest News</h2>
       <div className={styles['card-container']}>
         { renderedEntries }
+      </div>
+      <div className={styles['card-slider']}>
+        <CarouselProvider
+          naturalSlideWidth={90}
+          naturalSlideHeight={72}
+          totalSlides={newsEntries.length}
+        >
+          <Slider>
+            { renderedSlides }
+          </Slider>
+          <div className={styles['buttons']} >
+            <ButtonBack className={styles['button-back']}>
+              <FontAwesomeIcon className={styles['back-arrow']} icon={faCaretSquareLeft} size="2x"/>
+            </ButtonBack>
+            <DotGroup className={styles.dotgroup}/>
+            <ButtonNext className={styles['button-next']}>
+              <FontAwesomeIcon className={styles['next-arrow']} icon={faCaretSquareRight} size="2x"/>
+            </ButtonNext>
+          </div>
+        </CarouselProvider>
       </div>
       <Link href="/news" passHref>
         <a href="#ref" className={styles.button}>
