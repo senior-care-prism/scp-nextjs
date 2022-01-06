1ca35bc8bc04b9655ba80760dbdec2abfbd9ef65
Oussama El Zein <oussama.elzein@gmail.com>
2022-01-06 10:14:45 -0800

    Resource pdf links can now download (or open in new tab)

--- components/ResourceEntry.jsx
+++ components/ResourceEntry.jsx
@@ -3,8 +3,15 @@ import ShareWidget from './ShareWidget';
 import styles from '../styles/ResourceEntry.module.scss';
 import { formatDate } from '../lib/utils';
 import { ARTICLE_SHAPE } from '../shared/constants';
+import download from 'downloadjs';
 
 function ResourceEntry({ resource }) {
+
+  const handleDownload = (e) => {
+    const url = resource.downloadable.url;
+    const filename = url.substring(url.lastIndexOf('/')+1);
+    fetch(url).then(response => response.blob()).then(data => download(data, filename));
+  }
   return (
     <section className={styles['resource-entry']}>
       <div className={styles.logo}>
@@ -16,7 +23,10 @@ function ResourceEntry({ resource }) {
             <img className={styles.imagecontainer} src={resource.screenshot.url}/>
         )
         }
-        <a className={styles.link} href={resource.downloadable.url} target="_blank" rel="noreferrer" download>Download</a>
+        <div className={styles.files}>
+          <a className={styles.link} href={resource.downloadable.url} target="_blank" rel="noreferrer">Open</a>
+          <a className={styles.link} target="_blank" onClick={handleDownload} rel="noreferrer" download>Download</a>
+        </div>
       </div>
       <div className={styles.content}>
         <div className={styles.heading}>
